a337762905a7e1821c31dd26b6bae909e4070c5d
Skull <lccb92@gmail.com>
2024-01-18 21:10:14 -0300

    Initial commit

---
+++ src/FilamentBalanceHeaderServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace lukasccb\FilamentBalanceHeader;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class FilamentBalanceHeaderServiceProvider extends PackageServiceProvider
+{
+    public static string $name = 'filament-api-balance-header';
+
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name(static::$name)
+            ->hasViews();
+    }
+}
